4eea988c2ecdcdd8884675ebd23a49859a39bc64
Claude-Kamaha <kclaudejoelle20@gmail.com>
2021-04-19 17:46:43 +0100

    dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { DefaultComponent } from './layouts/default/default.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
